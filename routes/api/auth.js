427643dda8e750dc1d27ed6e3f7e6a1d0f7ea91d
Dan Retegan <danretegan@yahoo.com>
2024-05-26 17:45:56 +0300

    current user data

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -121,4 +121,24 @@ router.get("/logout", AuthController.validateAuth, async (req, res, next) => {
   }
 });
 
+// TODO GET /users/current
+router.get("/current", AuthController.validateAuth, async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    res.status(200).json({
+      email: user.email,
+      subscription: user.subscription,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 module.exports = router;
