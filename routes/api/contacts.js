d4eed916319198657ef5103e0ccc37ba03e4828e
Dan Retegan <danretegan@yahoo.com>
2024-05-26 16:59:06 +0300

    secure all routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,10 @@
-// TODO aici gestionăm operațiile CRUD (create, read, update, delete) și returnam un răspuns către client. Aici gestionăm interacțiunea cu utilizatorul final:
-
 require("../../passport.js");
 
 const express = require("express");
 const colors = require("colors");
-
 const { STATUS_CODES } = require("../../utils/constants.js");
-
 const AuthController = require("../../controllers/authController.js");
 
-// TODO importam funcțiile de manipulare a datelor:
 const {
   listContacts,
   getContactById,
@@ -21,7 +16,6 @@ const {
 
 const router = express.Router();
 
-// TODO Functia Respod With Error:
 const respondWithError = (res, error) => {
   console.error(colors.bgRed.italic.bold(error));
   res.status(STATUS_CODES.error).json({ message: `${error}` });
@@ -32,7 +26,6 @@ const respondWithError = (res, error) => {
 router.get("/", AuthController.validateAuth, async (req, res, next) => {
   try {
     const contacts = await listContacts();
-
     res
       .status(STATUS_CODES.success)
       .json({ message: "The list was successfully returned", data: contacts });
@@ -43,29 +36,30 @@ router.get("/", AuthController.validateAuth, async (req, res, next) => {
 
 // TODO GET id (GET Contact By Id):
 /* GET localhost:3000/api/contacts/:id */
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const contact = await getContactById(req.params.contactId);
-    console.log(req.params.contactId);
-
-    if (!contact) {
-      throw new Error("The contact was not found");
+router.get(
+  "/:contactId",
+  AuthController.validateAuth,
+  async (req, res, next) => {
+    try {
+      const contact = await getContactById(req.params.contactId);
+      console.log(req.params.contactId);
+      if (!contact) {
+        throw new Error("The contact was not found");
+      }
+      res.status(STATUS_CODES.success).json({
+        message: "The contact has been returned successfully",
+        data: contact,
+      });
+    } catch (error) {
+      respondWithError(res, error);
     }
-
-    res.status(STATUS_CODES.success).json({
-      message: "The contact has been returned successfully",
-      data: contact,
-    });
-  } catch (error) {
-    respondWithError(res, error);
   }
-});
+);
 
 // TODO POST (add /create):
 /* POST localhost:3000/api/contacts/ */
-router.post("/", async (req, res, next) => {
+router.post("/", AuthController.validateAuth, async (req, res, next) => {
   const { name, email, phone } = req.body;
-
   try {
     const newContact = await addContact({ name, email, phone });
     res.status(STATUS_CODES.created).json(newContact);
@@ -76,67 +70,72 @@ router.post("/", async (req, res, next) => {
 
 // TODO DELETE:
 /* DELETE localhost:3000/api/contacts/:id */
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const contactId = req.params.contactId;
-    const removedContact = await removeContact(contactId);
-
-    if (!removedContact) {
+router.delete(
+  "/:contactId",
+  AuthController.validateAuth,
+  async (req, res, next) => {
+    try {
+      const contactId = req.params.contactId;
+      const removedContact = await removeContact(contactId);
+      if (!removedContact) {
+        res
+          .status(STATUS_CODES.notFound)
+          .json({ message: "The contact was not found" });
+        return;
+      }
       res
-        .status(STATUS_CODES.notFound)
-        .json({ message: "The contact was not found" });
-      return;
+        .status(STATUS_CODES.deleted)
+        .json({ message: "Contact deleted successfully" });
+    } catch (error) {
+      respondWithError(res, error);
     }
-
-    res
-      .status(STATUS_CODES.deleted)
-      .json({ message: "Contact deleted successfully" });
-  } catch (error) {
-    respondWithError(res, error);
   }
-});
+);
 
 // TODO PUT (Update By Id):
 /* PUT localhost:3000/api/contacts/:id */
-router.put("/:contactId", async (req, res, next) => {
-  const contactId = req.params.contactId;
-
-  try {
-    const updatedContact = await updateContact(req.body, contactId);
-
-    if (!updatedContact) {
-      res
-        .status(STATUS_CODES.notFound)
-        .json({ message: "The contact was not found" });
-      return;
+router.put(
+  "/:contactId",
+  AuthController.validateAuth,
+  async (req, res, next) => {
+    const contactId = req.params.contactId;
+    try {
+      const updatedContact = await updateContact(req.body, contactId);
+      if (!updatedContact) {
+        res
+          .status(STATUS_CODES.notFound)
+          .json({ message: "The contact was not found" });
+        return;
+      }
+      res.status(STATUS_CODES.success).json(updatedContact);
+    } catch (error) {
+      respondWithError(res, error);
     }
-
-    res.status(STATUS_CODES.success).json(updatedContact);
-  } catch (error) {
-    respondWithError(res, error);
   }
-});
+);
 
 // TODO @ PATCH /api/contacts/:contactId/favorite
-router.patch("/:contactId/favorite", async (req, res) => {
-  const contactId = req.params.contactId;
-  const { favorite } = req.body;
-
-  if (favorite === undefined) {
-    return res
-      .status(STATUS_CODES.badRequest)
-      .json({ message: "missing field favorite" });
-  }
-
-  try {
-    const updatedContact = await updateStatusContact(contactId, { favorite });
-    if (!updatedContact) {
-      return res.status(STATUS_CODES.notFound).json({ message: "Not found" });
+router.patch(
+  "/:contactId/favorite",
+  AuthController.validateAuth,
+  async (req, res) => {
+    const contactId = req.params.contactId;
+    const { favorite } = req.body;
+    if (favorite === undefined) {
+      return res
+        .status(STATUS_CODES.badRequest)
+        .json({ message: "missing field favorite" });
     }
-    res.status(STATUS_CODES.success).json(updatedContact);
-  } catch (error) {
-    respondWithError(res, error);
+    try {
+      const updatedContact = await updateStatusContact(contactId, { favorite });
+      if (!updatedContact) {
+        return res.status(STATUS_CODES.notFound).json({ message: "Not found" });
+      }
+      res.status(STATUS_CODES.success).json(updatedContact);
+    } catch (error) {
+      respondWithError(res, error);
+    }
   }
-});
+);
 
 module.exports = router;
